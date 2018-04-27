9be4b68da7ed2f201875b8c265c6ffac2616ca8f
AsianIncest <avsv3@ya.ru>
2018-04-27 04:44:00 +0300

    test update script

--- src/smarthome.lua
+++ src/smarthome.lua
@@ -1,3 +1,11 @@
 --проверка работы
 print("latin")
 print("кириллица")
+
+local comp = require("component")
+--local inet = comp.internet
+local inet = require("internet")
+local b = inet.request("https://github.com/AsianIncest/OC/raw/master/src/smarthome.lua")
+for line in b do
+print(line)
+end
