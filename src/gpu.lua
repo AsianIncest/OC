fc591f84dbda502d403b1c7e1fc5942aa026062a
AsianIncest <avsv3@ya.ru>
2018-04-27 05:20:33 +0300

    нажатия кнопок

--- src/gpu.lua
+++ src/gpu.lua
@@ -1,12 +1,14 @@
-local component = require('component')
-local gpu = component.gpu
-local color1, color2, text = 0xff00ff, 0x0000ff, 'Hello, OpenComputers!'
-for i = 1, 10 do
-  gpu.setForeground(color1)
-  gpu.setBackground(color2)
-  gpu.set(10, 5, text)
-  color1, color2 = color2, color1
-  os.sleep(0.5)
+local gpu = require('component').gpu
+local pull_e = require('event').pull
+local b_color = gpu.getBackground()
+local color = {[0] = 0x00ff00, 0x0000ff}
+while true do
+  local tEvent = {pull_e('touch')}
+  if tEvent[3] == 1 and tEvent[4] == 1 then
+    gpu.setBackground(b_color)
+    os.exit()
+  end
+  gpu.setBackground(color[tEvent[5]])
+  gpu.set(tEvent[3], tEvent[4], ' ')
+  gpu.set(1, 1, 'x: '..tEvent[3]..' y: '..tEvent[4]..'    ')
 end
-gpu.setForeground(0xffffff)
-gpu.setBackground(0)
